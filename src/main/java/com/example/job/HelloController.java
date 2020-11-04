60209a68d664f82c9e38729e92a7539d8ffb1a36
ljadanowski <lukasz.jadanowski@gmail.com>
2020-11-04 12:11:04 +0100

    Jeden commit

---
+++ src/main/java/com/example/job/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.job;
+
+public class HelloController {
+    public void aa() {
+
+    }
+
+    public void bb() {
+
+    }
+
+    public void cc() {
+
+    }
+
+    public void dd() {
+
+    }
+}
